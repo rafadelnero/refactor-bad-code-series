b6a05734547e2753c77a8b3187f85d61d1038844
rafaDelNero <rafacdelnero@gmail.com>
2018-03-05 23:41:03 +0000

    Util class from Robin.

--- refactorbadcode/src/main/java/com/refactorbadcode/Util.java
+++ refactorbadcode/src/main/java/com/refactorbadcode/Util.java
@@ -1,7 +1,20 @@
 package com.refactorbadcode;
 
+import com.refactorbadcode.model.Product;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Util {
 
-    
+    public static String formatPhone(String number) { return null; }
+
+    public static Date formatDate(String date) {return null;}
+
+    public static BigDecimal calculateProductPrice(Product product) {return null;}
+
+    public static String convertToInteger(String anyString) {return null;}
+
+    // More tons of other methods
 
 }
